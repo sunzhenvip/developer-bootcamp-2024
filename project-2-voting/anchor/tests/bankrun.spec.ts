f4b58b1652944d70d38ab042c2db10c45cddc018
test <test@qq.com>
2025-06-20 10:43:51 +0800

    x1

---
+++ project-2-voting/anchor/tests/bankrun.spec.ts
@@ -0,0 +1,52 @@
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+import { PublicKey,Keypair } from '@solana/web3.js';
+import * as anchor from '@coral-xyz/anchor';
+import { BN, Program } from "@coral-xyz/anchor";
+
+
+const IDL = require("../target/idl/voting.json");
+import { Voting } from '../target/types/voting';
+
+const PUPPET_PROGRAM_ID = new PublicKey("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
+
+describe('Create a system account', () => {
+
+  it("bankrun", async () => {
+    const context = await startAnchor("", [{name: "voting", programId: PUPPET_PROGRAM_ID}], []);
+    const provider = new BankrunProvider(context);
+
+
+    const provider_ar = anchor.AnchorProvider.env();
+    // console.log("provider_ar", provider_ar);
+
+    const payer_ar = provider_ar.wallet as anchor.Wallet;
+    console.log("payer_ar", payer_ar.publicKey.toString());
+
+    const counterKeypair = Keypair.generate();
+    console.log("counterKeypair", counterKeypair);
+
+    const puppetProgram = new Program<Voting>(
+      IDL,
+      provider,
+    );
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("poll"), new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      puppetProgram.programId
+    );
+
+    await puppetProgram.methods.initializePoll(
+      new anchor.BN(1),
+        new anchor.BN(0),
+        new anchor.BN(1759508293),
+        "test-poll",
+        "description",
+    ).rpc();
+
+    const pollAccount = await puppetProgram.account.pollAccount.fetch(pollAddress);
+    console.log(123,pollAccount);
+
+  });
+
+});
