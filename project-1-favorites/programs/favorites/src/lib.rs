35ed35a2981af0477f03caa91d9db749fe4ca696
test <test@qq.com>
2025-08-15 21:29:40 +0800

    project-1-favorites update

--- project-1-favorites/programs/favorites/src/lib.rs
+++ project-1-favorites/programs/favorites/src/lib.rs
@@ -6,28 +6,28 @@ declare_id!("ww9C83noARSQVBnqmCUmaVdbJjmiwcV9j2LkXYMoUCV");
 // Anchor programs always use 8 bits for the discriminator
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
-// Our Solana program! 
+// Our Solana program!
 #[program]
 pub mod favorites {
     use super::*;
 
     // Our instruction handler! It sets the user's favorite number and color
-    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies: Vec<String>) -> Result<()> {
+    pub fn set_favorites(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
         let user_public_key = context.accounts.user.key();
         msg!("Greetings from {}", context.program_id);
-        msg!(
-            "User {user_public_key}'s favorite number is {number}, favorite color is: {color}",
-        );
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is: {color}",);
 
-        msg!(
-            "User's hobbies are: {:?}",
-            hobbies
-        ); 
+        msg!("User's hobbies are: {:?}", hobbies);
 
         context.accounts.favorites.set_inner(Favorites {
             number,
             color,
-            hobbies
+            hobbies,
         });
         Ok(())
     }
@@ -45,7 +45,7 @@ pub struct Favorites {
     pub color: String,
 
     #[max_len(5, 50)]
-    pub hobbies: Vec<String>
+    pub hobbies: Vec<String>,
 }
 // When people call the set_favorites instruction, they will need to provide the accounts that will be modifed. This keeps Solana fast!
 #[derive(Accounts)]
@@ -54,9 +54,9 @@ pub struct SetFavorites<'info> {
     pub user: Signer<'info>,
 
     #[account(
-        init_if_needed, 
-        payer = user, 
-        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
         seeds=[b"favorites", user.key().as_ref()],
     bump)]
     pub favorites: Account<'info, Favorites>,
