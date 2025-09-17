fcbaa24b1ebf06010f4742d15820470ffd448423
test <test@qq.com>
2025-09-17 10:50:37 +0800

    project-6 update

--- project-6-nfts/verify-nft.ts
+++ project-6-nfts/verify-nft.ts
@@ -14,6 +14,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 
 import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
 import { keypairIdentity, publicKey } from "@metaplex-foundation/umi";
+import {base58} from "@metaplex-foundation/umi-serializers";
 
 const connection = new Connection(clusterApiUrl("devnet"));
 
@@ -38,10 +39,10 @@ console.log("Set up Umi instance for user");
 
 // We could also dp
 const collectionAddress = publicKey(
-  "ChfGtd2wT12c2u82PHNpe4PdQ5PMqJnVECfaNbQ2uaVw"
+  "7h3DckfS79EtKvWUonEbwEsaetXc7uj6QWUt9TELbrg4"
 );
 
-const nftAddress = publicKey("84uZmnRpzfa77w3KtyagjWXA8g1yE6vP8ReuHyN5HZaN");
+const nftAddress = publicKey("F3CQRR9XDNASMxt4YcRBqHWxLJFE5hYxu3KWvCKXx55P");
 
 const transaction = await verifyCollectionV1(umi, {
   metadata: findMetadataPda(umi, { mint: nftAddress }),
@@ -49,7 +50,13 @@ const transaction = await verifyCollectionV1(umi, {
   authority: umi.identity,
 });
 
-transaction.sendAndConfirm(umi);
+let result = await transaction.sendAndConfirm(umi);
+
+const txSignature = base58.deserialize(result.signature)[0];
+console.log("verifyCollectionV1 signature ", txSignature);
+
+console.log(`稍等 20 秒钟加载 获取链上数据.....`);
+await sleep(20_000); // 等待 10000 毫秒 = 2 秒
 
 console.log(
   `✅ NFT ${nftAddress} verified as member of collection ${collectionAddress}! See Explorer at ${getExplorerLink(
@@ -58,3 +65,7 @@ console.log(
     "devnet"
   )}`
 );
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
